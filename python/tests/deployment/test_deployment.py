1ffb59ae7cf203d4d6414d59c63c14d1d6d55fbb
Ben Knoll <benknoll@umn.edu>
2023-11-03 12:00:49 -0500

    Fixed an issue with test_deployment.py (#241)

--- python/tests/deployment/test_deployment.py
+++ python/tests/deployment/test_deployment.py
@@ -1,16 +1,3 @@
-#  Copyright 2022 Regents of the University of Minnesota.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 import os
 import sys
 import threading
@@ -20,7 +7,7 @@ from subprocess import Popen, PIPE, STDOUT, run
 from tempfile import TemporaryDirectory
 
 import pytest
-from mtap.serialization import YamlSerializer
+from mtap.serialization import JsonSerializer
 
 
 @pytest.fixture(name='deploy_all', scope='module')
@@ -77,7 +64,7 @@ def test_deploy_run(deploy_all, processor_timeout):
                  timeout=processor_timeout, stdout=PIPE, stderr=STDOUT)
         print(cp.stdout.decode('utf-8'), end='')
         assert cp.returncode == 0
-        with YamlSerializer.file_to_event(Path(tmpdir) / '97_204.txt.json') as event:
+        with JsonSerializer.file_to_event(Path(tmpdir) / '97_204.txt.json') as event:
             document = event.documents['plaintext']
             assert len(document.labels['sentences']) > 0
             assert len(document.labels['pos_tags']) > 0
@@ -95,7 +82,7 @@ def test_deploy_run_rtf(deploy_all, processor_timeout):
                  timeout=processor_timeout, stdout=PIPE, stderr=STDOUT)
         print(cp.stdout.decode('utf-8'), end='')
         assert cp.returncode == 0
-        with YamlSerializer.file_to_event(Path(tmpdir) / '97_204.rtf.json') as event:
+        with JsonSerializer.file_to_event(Path(tmpdir) / '97_204.rtf.json') as event:
             document = event.documents['plaintext']
             assert len(document.labels['sentences']) > 0
             assert len(document.labels['pos_tags']) > 0
